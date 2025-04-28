bfedd2e8cfabc972fceff879b5b8b8ab8f76f0c8
Kevinsbl <u20231214185@usco.edu.co>
2025-04-27 23:16:14 -0500

    Se implementaron cambios en navbar, aun esta pendiente hacer funcionar de manera correcta el despliegue de los submenus usuarios, categorias y productos

--- inc/navbar.php
+++ inc/navbar.php
@@ -3,21 +3,20 @@
     <a class="navbar-item" href="index.php?vista=crear_usuario">
       <img src="./img/libros.png" alt="" width="40" height="50">
     </a>
-
+    <p class="navbar-item has-text-white is-size-6">Sistema de Gestion Educativo SAS</p>
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
-    <p class="navbar-item has-text-white">Sistema de Gestion Educativo SAS</p>
   </div>
 
-  <!-- <div id="navbarBasicExample" class="navbar-menu">
+  <div id="navbarBasicExample" class="navbar-menu ">
     <div class="navbar-start">
-      <div class="navbar-item has-dropdown is-hoverable">
+      <div class="navbar-item has-dropdown is-hoverable ">
         <a class="navbar-link">
-          Usuarios  
+          Usuarios
         </a>
         <div class="navbar-dropdown">
           <a class="navbar-item">
@@ -35,7 +34,6 @@
         <a class="navbar-link">
           Categorias
         </a>
-
         <div class="navbar-dropdown">
           <a class="navbar-item">
             Nuevo
@@ -46,14 +44,12 @@
           <a class="navbar-item">
             Buscar
           </a>
-
         </div>
       </div>
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">
           Productos
         </a>
-
         <div class="navbar-dropdown">
           <a class="navbar-item">
             Nuevo
@@ -62,60 +58,58 @@
             Lista
           </a>
           <a class="navbar-item">
-            por Categorias
-          </a>
-          <a class="navbar-item">
             Buscar
           </a>
-
         </div>
       </div>
-    </div> -->
+    </div>
 
-  <div class="navbar-end is-rounded">
-    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          <span class="icon-text">
-            <span class="icon">
-              <i class="fas fa-user"></i>
-            </span>
-            <span><?= htmlspecialchars($_SESSION['username']) ?></span>
-            <span class="tag is-light ml-2"><?= $_SESSION['rol'] ?></span>
-          </span>
-        </a>
-        <div class="navbar-dropdown">
-          <a class="navbar-item" href="index.php?vista=session_info">
+    <div class="navbar-end">
+      <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link has-text-white">
             <span class="icon-text">
               <span class="icon">
-                <i class="fas fa-info-circle"></i>
+                <i class="fas fa-user"></i>
               </span>
-              <span>Ver sesión</span>
+              <span><?= htmlspecialchars($_SESSION['username']) ?></span>
+              <span class="tag is-light ml-2"><?= $_SESSION['rol'] ?></span>
             </span>
           </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item" href="procesos/usuarios/logout.php">
-            <span class="icon-text">
-              <span class="icon">
-                <i class="fas fa-sign-out-alt"></i>
-              </span>
-              <span>Cerrar sesión</span>
-            </span>
-          </a>
+          <div class="navbar-dropdown">
+            <div class="navbar-dropdown">
+              <a class="navbar-item" href="index.php?vista=session_info">
+                <span class="icon-text">
+                  <span class="icon">
+                    <i class="fas fa-info-circle"></i>
+                  </span>
+                  <span>Ver sesión</span>
+                </span>
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item" href="procesos/usuarios/logout.php">
+                <span class="icon-text">
+                  <span class="icon">
+                    <i class="fas fa-sign-out-alt"></i>
+                  </span>
+                  <span>Cerrar sesión</span>
+                </span>
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    <?php else: ?>
-
-      <div class="navbar-end">
+      <?php else: ?>
         <div class="navbar-item">
-          <p class="button is-link is-rounded">
-            <a href="index.php?vista=login" class="has-text-light">
+          <div class="buttons">
+            <a class="button" href="index.php?vista=login">
               Iniciar sesión
             </a>
-          </p>
+            <a class="button is-link" href="">
+              Registrate
+            </a>
+          </div>
         </div>
-
-        </p>
       <?php endif; ?>
-      </div>
+    </div>
+  </div>
 </nav>
--- inc/script.php
+++ inc/script.php
@@ -8,14 +8,22 @@
         $navbarBurgers.forEach(el => {
             el.addEventListener('click', () => {
 
-                // Get the target from the "data-target" attribute
                 const target = el.dataset.target;
                 const $target = document.getElementById(target);
 
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
                 el.classList.toggle('is-active');
                 $target.classList.toggle('is-active');
 
+            });
+        });
+        const $dropdownToggles = Array.prototype.slice.call(document.querySelectorAll('.navbar-link'), 0);
+        $dropdownToggles.forEach(el => {
+            el.addEventListener('click', (e) => {
+                if (window.innerWidth <= 1023) { 
+                    e.preventDefault(); 
+                    const dropdown = el.closest('.navbar-item').querySelector('.navbar-dropdown');
+                    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+                }
             });
         });
 
